4d646bfaa8724d8631f39e5400d361bac66b9c58
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-04-04 01:51:32 +0200

    unit tests added

---
+++ TagFetcherTests/TagServiceUnitTests/TagServiceUnitTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagFetcherInfrastructure.data;
+using TagFetcherInfrastructure.interfaces;
+using TagFetcherInfrastructure.queryParamsModels;
+using TagFetcherInfrastructure.services;
+using Xunit;
+
+namespace TagFetcherTests.TagServiceUnitTests
+{
+    public class TagServiceUnitTests
+    {
+        private readonly ITagService _service;
+        private readonly AppDbContext _context;
+
+        public TagServiceUnitTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            _context = new AppDbContext(options);
+            _service = new TagService(_context);
+
+            TestDataSeeder.SeedDatabase(_context);
+        }
+
+        [Fact]
+        public async Task GetAllTagsFromDbAsync_ReturnsAllTags()
+        {
+            // Arrange - check if there are two tags in the in-memory database
+            var initialCount = await _context.Tags.CountAsync();
+            Assert.Equal(2, initialCount);
+
+            // Act - get all tags from the in-memory database
+            var result = await _service.GetAllTagsFromDbAsync();
+
+            // Assert - check if all tags are returned
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async Task DeleteAllTagsAsync_DeletesAllTags()
+        {
+            // Arrange
+            var initialCount = await _context.Tags.CountAsync();
+            Assert.Equal(2, initialCount);
+
+            // Act
+            await _service.DeleteAllTagsAsync();
+
+            // Assert
+            var countAfterDeletion = await _context.Tags.CountAsync();
+            Assert.Equal(0, countAfterDeletion);
+        }
+
+        [Fact]
+        public async Task GetTagsAsync_SortsAndPaginatesCorrectly()
+        {
+            // Arrange
+            var queryParameters = new TagsQueryParameters
+            {
+                PageSize = 1,
+                PageNumber = 2,
+                SortBy = "name",
+                SortOrder = "asc"
+            };
+
+            // Act
+            var result = await _service.GetTagsAsync(queryParameters);
+
+            // Assert
+            Assert.Single(result);
+            Assert.Equal("Test Tag 2", result.First().Name);
+        }
+
+    }
+}
---
+++ TagFetcherTests/TagServiceUnitTests/TestDataSeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagFetcherDomain.models;
+using TagFetcherInfrastructure.data;
+
+namespace TagFetcherTests.TagServiceUnitTests
+{
+    public static class TestDataSeeder
+    {
+        public static void SeedDatabase(AppDbContext context)
+        {
+            context.Tags.RemoveRange(context.Tags);
+            context.SaveChanges();
+
+            var tags = new List<Tag>
+            {
+            new Tag { Name = "Test Tag 1", Count = 1 },
+            new Tag { Name = "Test Tag 2", Count = 2 }
+            };
+
+            context.Tags.AddRange(tags);
+            context.SaveChanges();
+        }
+    }
+}
