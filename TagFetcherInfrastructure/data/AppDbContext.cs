c61d2704abdb5142f890e7ee9b5eb1095a96e6e0
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-04-04 01:20:32 +0200

    removed virtual keyword

--- TagFetcherInfrastructure/data/AppDbContext.cs
+++ TagFetcherInfrastructure/data/AppDbContext.cs
@@ -6,6 +6,6 @@ namespace TagFetcherInfrastructure.data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
-        public virtual DbSet<Tag> Tags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
     }
 }
